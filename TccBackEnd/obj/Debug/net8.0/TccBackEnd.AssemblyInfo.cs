747582a2f0c83edd43fe766c86dfd5481ee7fb14
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2025-02-19 12:11:14 +0100

    --

--- TccBackEnd/obj/Debug/net8.0/TccBackEnd.AssemblyInfo.cs
+++ TccBackEnd/obj/Debug/net8.0/TccBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db221382e01638cdcdb5f56a9c26591f95411377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085e76db64958665e835ea2ef8c93f4dfff9c3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
