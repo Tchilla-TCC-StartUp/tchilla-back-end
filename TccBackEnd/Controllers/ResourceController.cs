90353594c086da69cf6a9b116252ce56849e91a9
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2025-02-24 00:46:51 +0100

    (feat): adicionado onboarding and welcome endpoints in ResourceController

--- TccBackEnd/Controllers/ResourceController.cs
+++ TccBackEnd/Controllers/ResourceController.cs
@@ -12,27 +12,52 @@ namespace TccBackEnd.Controllers;
 public class ResourceController : Controller
 {
   private readonly ILogger<ResourceController> _logger;
-  private readonly IWebHostEnvironment _env;
   public ResourceController(ILogger<ResourceController> logger)
   {
     _logger = logger;
   }
+  [HttpGet("welcome")]
+  public IActionResult Welcome()
+  {
+    string baseUrl = $"{Request.Scheme}://{Request.Host}/Resources/images/";
+
+    object welcome = new 
+    {
+      titulo = "Seja bem vindo ao Tchilla",
+      url = $"{baseUrl}Welcome-cuate 1.png",
+      descricao = "Estamos grato por ter baixado o nosso aplicativo, crie conta, faz login, ou explore a App."
+    };
+    
+    return Ok(welcome);    
+  }
 
   [HttpGet("onboarding")]
-  public async Task<IActionResult> OnBoarding()
+  public IActionResult OnBoarding()
   {
-    string folderPath = Path.Combine(_env.WebRootPath, "Resources/images");
+    string baseUrl = $"{Request.Scheme}://{Request.Host}/Resources/images/";
 
-        if (!Directory.Exists(folderPath))
-            return NotFound("A pasta de imagens não existe.");
-
-        var images = Directory.GetFiles(folderPath)
-            .Select(file => new
-            {
-                nome = Path.GetFileName(file),
-                url = $"{Request.Scheme}://{Request.Host}/Resources/images/{Path.GetFileName(file)}"
-            });
-
-        return Ok(images);
+    List<object> onboardingSlides = new List<object> {
+      new 
+      {
+        titulo = "Economize tempo na sua busca.",
+        url = $"{baseUrl}House searching-cuate.png",
+        descricao = "Com apenas alguns cliques, encontre o local ideal para qualquer evento social com as melhores opcoes "
+      },
+      new 
+      {
+        titulo = "Servicos de decoracao no pacote",
+        url = $"{baseUrl}Weddingplanner-cuate.png",
+        descricao = "Take advantage of the moment to reserve what you want to eat today, don’t wait for tomorrow."
+      },
+      new 
+      {
+        titulo = "Compare, avalie  com confianca",
+        url = $"{baseUrl}Wedding-cuate.png",
+        descricao = "Take advantage of the moment to reserve what you want to eat today, don’t wait for tomorrow."
+      }
+    };  
+    
+    
+    return Ok(onboardingSlides);    
   }
 }
