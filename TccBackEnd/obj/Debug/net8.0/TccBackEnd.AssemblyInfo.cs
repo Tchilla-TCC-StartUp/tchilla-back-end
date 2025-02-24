9eb4fccc93a7a1e20d597a891ff43cb6b288934d
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2025-02-24 10:28:02 +0100

    fix: switched to svg images

--- TccBackEnd/obj/Debug/net8.0/TccBackEnd.AssemblyInfo.cs
+++ TccBackEnd/obj/Debug/net8.0/TccBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ca16e8804f54bcefe89ea52d0b9cc1f4e04797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01da73763b372036a3d65b9a4a6f8a2409c59e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
