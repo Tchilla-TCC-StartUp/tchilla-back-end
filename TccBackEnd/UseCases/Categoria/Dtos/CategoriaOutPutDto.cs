9d2a0160d0a490ec85b5b5a97988e684ab17e60b
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2025-04-09 23:25:34 +0100

    fix: getAll Categorias

--- TccBackEnd/UseCases/Categoria/Dtos/CategoriaOutPutDto.cs
+++ TccBackEnd/UseCases/Categoria/Dtos/CategoriaOutPutDto.cs
@@ -2,6 +2,7 @@ namespace TccBackEnd.UseCases.Categoria.Dtos;
 
 public class CategoriaOutPutDto
 {
+    public int Id {get;set;}
     public string Nome { get; set; }
     public string Descricao { get; set; }
 }
