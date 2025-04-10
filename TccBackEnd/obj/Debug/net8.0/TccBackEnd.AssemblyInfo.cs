7163339f218668578410809aa4a30b40363a7ffc
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2025-04-10 01:08:13 +0100

    merge main

--- TccBackEnd/obj/Debug/net8.0/TccBackEnd.AssemblyInfo.cs
+++ TccBackEnd/obj/Debug/net8.0/TccBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2a0160d0a490ec85b5b5a97988e684ab17e60b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e4d778464a07f09b1b4e5f97456cb92a323639e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TccBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
